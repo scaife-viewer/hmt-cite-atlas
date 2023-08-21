a6b5b9f74193caff73156cc1e08cd4678af98df5
Jacob Wegner <jacobwegner@gmail.com>
2023-08-21 16:43:40 -0500

    Backport PRAGMA directives from scaife-viewer-atlas package

--- hmt_cite_atlas/library/apps.py
+++ hmt_cite_atlas/library/apps.py
@@ -1,5 +1,20 @@
-from django.apps import AppConfig
+from django.apps import AppConfig as BaseAppConfig
+from django.db.backends.signals import connection_created
 
 
-class LibraryConfig(AppConfig):
+class LibraryConfig(BaseAppConfig):
     name = "library"
+
+
+def tweak_sqlite_pragma(sender, connection, **kwargs):
+    """
+    Customize PRAGMA settings for SQLite
+    """
+    if connection.vendor == "sqlite":
+        cursor = connection.cursor()
+        cursor.execute("PRAGMA synchronous=OFF;")
+        cursor.execute("PRAGMA cache_size=100000;")
+        cursor.execute("PRAGMA journal_mode=MEMORY;")
+
+
+connection_created.connect(tweak_sqlite_pragma)
